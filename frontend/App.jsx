a7cbb99aa5e0e1d776a30d03193d363d2f02258b
Avijit Das <rdas9237@gmail.com>
2024-10-30 22:38:54 +0530

    Adding something

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -5,15 +5,7 @@ import './App.css'
 import MyAceEditor from './src/Editor';
 
 function App() {
-  // const editorRef = useRef(null);
-  // const [count, setCount] = useState(0)
-  // const notify = () => toast("Wow so easy!");
-  // function handleEditorDidMount(editor, monaco) {
-  //   editorRef.current = editor;
-  // }
-  // function showValue() {
-  //   alert((editorRef.current.getValue()).trim());
-  // }
+
   return (
     <>
 <MyAceEditor/>
